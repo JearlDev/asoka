fff6a59f24b1f7525334fea9b907c57a356ae9f3
Josh Early <joshe@Joshs-MBP.home>
2024-04-08 08:29:40 +0200

    buttons complete

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,50 +4,45 @@ import './Header.css';
 const Header = () => {
   return (
     <>
-      <h1 className="heading heading--lg text-primary">Heading</h1>
-      <h2 className="heading heading--md text--secondary">Heading</h2>
-      <h3 className="heading heading--sm text-secondary">Heading</h3>
-      <p className="bodyText text-bodyDark">
-        Lorem ipsum dolor sit amet consectetur <a href="#">adipisicing elit.</a>{' '}
-        Harum omnis, dignissimos porro ipsam saepe distinctio quia in soluta
-        veniam itaque fuga nesciunt dolor tenetur vitae labore tempore aliquam
-        repellendus perspiciatis.
-      </p>
-      <div className="btn-wrapper flex gap-2 justify-start">
-        <a href="#" className="btn btn--underline btn--underlineDark">
-          <div className="btn--underline__inner">
-            <span>Learn More</span>
-          </div>
-          <div className="btn--underline__inner overlayText">
-            <span>Learn More</span>
-          </div>
-        </a>
+      <div className="wrapper bg-secondary">
+        <h1 className="heading heading--lg text-primary">Heading</h1>
+        <h2 className="heading heading--md text--secondary">Heading</h2>
+        <h3 className="heading heading--sm text-secondary">Heading</h3>
+        <p className="bodyText text-bodyDark">
+          Lorem ipsum dolor sit amet consectetur{' '}
+          <a href="#">adipisicing elit.</a> Harum omnis, dignissimos porro ipsam
+          saepe distinctio quia in soluta veniam itaque fuga nesciunt dolor
+          tenetur vitae labore tempore aliquam repellendus perspiciatis.
+        </p>
+        {/* <div className="btn-wrapper flex gap-2 justify-start">
+          <a href="#" className="btn btn--underline btn--underlineDark">
+            <div className="btn--underline__inner">
+              <span>Learn More</span>
+            </div>
+            <div className="btn--underline__inner overlayText">
+              <span>Learn More</span>
+            </div>
+          </a>
+        </div> */}
+        {/* <div className="btn-wrapper flex gap-2 justify-start">
+          <a href="#" className="btn btn--underline btn--underlineLight">
+            <div className="btn--underline__inner">
+              <span>Learn More</span>
+            </div>
+            <div className="btn--underline__inner overlayText">
+              <span>Learn More</span>
+            </div>
+          </a>
+        </div> */}
+        <div className="btn-wrapper flex gap-2 justify-start mt-10">
+          <a href="#" className="btn btn--rounded btn--roundedGold">
+            <span className="btn--rounded__text">Learn More</span>
+          </a>
+          <a href="#" className="btn btn--rounded btn--roundedWhite">
+            <span className="btn--rounded__text">Learn More</span>
+          </a>
+        </div>
       </div>
-      <div className="btn-wrapper flex gap-2 justify-start">
-        <a href="#" className="btn btn--underline btn--underlineLight">
-          <div className="btn--underline__inner">
-            <span>Learn More</span>
-          </div>
-          <div className="btn--underline__inner overlayText">
-            <span>Learn More</span>
-          </div>
-        </a>
-      </div>
-      {/* <div className="btn-wrapper flex gap-2 justify-start mt-10">
-        <a href="#" className="btn btn--underline btn--underlineLight">
-          Learn More
-        </a>
-      </div>
-      <div className="btn-wrapper flex gap-2 justify-start mt-10">
-        <a href="#" className="btn btn--rounded btn-roundedWhite">
-          Learn More
-        </a>
-      </div>
-      <div className="btn-wrapper flex gap-2 justify-start mt-10">
-        <a href="#" className="btn btn--rounded btn-roundedGold">
-          Learn More
-        </a>
-      </div> */}
     </>
   );
 };
