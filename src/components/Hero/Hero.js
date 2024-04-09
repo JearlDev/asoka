8b17c4f69f1e66769d9b08080dac0a56e6c5c1f3
Josh Early <joshe@Joshs-MBP.home>
2024-04-09 09:01:22 +0200

    hero initial setup

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,10 +1,134 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+
 import './Hero.css';
 
 const Hero = () => {
+  const [videoModalIsOpen, setVideoModalIsOpen] = useState(false);
+
+  const data = {
+    videoUrl: '/videos/asoka-homepage.mov',
+    imageUrl: '/images/hero-bg.webp',
+  };
+
+  useEffect(() => {
+    if (data.videoUrl) {
+      // open lightbox
+      if (videoModalIsOpen) {
+        const videoBtn = document.querySelector('.video-modal-btn');
+        const video = document.querySelector('.video-modal video');
+        const videoModal = document.querySelector('.video-modal');
+        const videoClose = document.querySelector('.video-modal__close');
+
+        // set video src
+        video.src = videoBtn
+          .querySelector('#video-src')
+          .getAttribute('data-src');
+
+        // show lightbox
+        videoModal.style.display = 'block';
+        setTimeout(function () {
+          videoModal.style.opacity = '100%';
+        }, 10);
+        setTimeout(function () {
+          video.style.opacity = '100%';
+          video.style.transform = 'scale(1,1)';
+        }, 140);
+        setTimeout(function () {
+          videoClose.style.opacity = '100%';
+          videoClose.style.transform = 'translateX(0)';
+        }, 350);
+
+        video.play();
+      } else {
+        // close lightbox
+        const video = document.querySelector('.video-modal video');
+        const videoModal = document.querySelector('.video-modal');
+        const videoClose = document.querySelector('.video-modal__close');
+        document.querySelector('.video-modal');
+
+        // hide lightbox
+        // videoClose.style.transform = 'translateX(10%)';
+        // videoClose.style.opacity = '0';
+
+        setTimeout(function () {
+          video.style.opacity = '0';
+          video.style.transform = 'scale(0.9,0.9)';
+        }, 200);
+        setTimeout(function () {
+          videoModal.style.opacity = '0';
+        }, 300);
+        setTimeout(function () {
+          videoModal.style.display = 'none';
+          document.body.style.overflow = 'visible';
+        }, 600);
+
+        video.pause();
+      }
+    }
+  }, [videoModalIsOpen, data.videoUrl]);
   return (
     <>
-      <h1>Hero</h1>
+      <section className="hero h-screen w-full relative flex flex-col items-center">
+        <a href="/" className="relative z-[20]">
+          <img
+            src="/images/logo-white.svg"
+            alt=""
+            className="logo logo--hero object-contain h-[150px] mt-25"
+          />
+        </a>
+        <img
+          src={data.imageUrl}
+          alt=""
+          className="bg-img bg-img--hero absolute top-0 left-0 object-cover h-full w-full"
+        />
+        <div className="video-modal-btn-wrapper absolute z-10 h-full w-full flex items-center justify-center">
+          <button
+            onClick={() => {
+              document.body.style.overflow = 'hidden';
+              setVideoModalIsOpen(true);
+            }}
+            className="video-modal-btn flex gap-3 items-center after:content-[''] after:absolute after:h-full after:w-full after:top-0 after:left-0 after:z-[10] hover:after:bg-black/50 after:pointer-events-none after:transition-all after:duration-300 after:ease-out group"
+          >
+            <img
+              src="/images/watch-video-icon.svg"
+              alt=""
+              className="video-modal-btn__icon relative z-[20] group-hover:scale-[1.5] group-hover:translate-x-[50.45px] transition-all duration-300 ease-out"
+            />
+            <span className="video-modal-btn__text btn text-white relative z-[20] group-hover:opacity-0 group-hover:translate-x-[-15px] transition-all duration-300 ease-out">
+              Watch Video
+            </span>
+            <span
+              id="video-src"
+              className="hidden"
+              data-src={data.videoUrl}
+            ></span>
+          </button>
+        </div>
+      </section>
+
+      {/* {videoModalIsOpen && ( */}
+      <section
+        className="video-modal fixed h-screen w-full top-0 right-0 z-[99999999] bg-black/70"
+        style={{ display: 'none' }}
+      >
+        <div
+          className="video-modal__inner h-full w-full flex items-center justify-center"
+          onClick={() => {
+            setVideoModalIsOpen(false);
+          }}
+        >
+          <button className="video-modal__close flex items-end font-normal text-14 transition-all duration-150 ease-out absolute top-[24px] right-[28px] z-50 py-1 hover:cursor-pointer">
+            <img src="" alt="" className="video-modal__close__icon" />
+            <span className="menu-modal__close__text">Close</span>
+          </button>
+          <video
+            className="h-[42vw] w-[82vw] -md:h-[70vh] -md:w-[90vw] overflow-hidden object-cover bg-transparent rounded-[10px]"
+            controls
+            type="video/mp4"
+          ></video>
+        </div>
+      </section>
+      {/* )} */}
     </>
   );
 };
