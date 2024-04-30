dbd9e29930628328cb1859010449cacdd2894e8a
Josh Early <joshe@Joshs-MBP.home>
2024-04-30 09:00:43 +0200

    menu modal data added and html complete

--- src/components/MenuModal/MenuModal.js
+++ src/components/MenuModal/MenuModal.js
@@ -11,6 +11,74 @@ const MenuModal = () => {
   const [sidebarIsShown, setSidebarIsShown] = useState(true);
   const [sidebarIsFixed, setSidebarIsFixed] = useState(false);
 
+  const data = {
+    patternImageUrl: '/images/bg-pattern-small.webp',
+    stayImageUrl: '/images/bg-stay.webp',
+    storyImageUrl: '/images/bg-story.webp',
+    surfingImageUrl: '/images/bg-surfing.webp',
+    diningImageUrl: '/images/bg-dining.webp',
+    wellnessImageUrl: '/images/bg-wellness.webp',
+    primaryMenu: [
+      {
+        url: '#stay',
+        title: 'Your Stay',
+      },
+      {
+        url: '#story',
+        title: 'Our Story',
+      },
+      {
+        url: '#surfing',
+        title: 'Surfing',
+      },
+      {
+        url: '#dining',
+        title: 'Dining',
+      },
+      {
+        url: '#wellness',
+        title: 'Wellness',
+      },
+    ],
+    secondaryMenu: [
+      {
+        url: '#mountain-villa',
+        title: 'Mountain Villa',
+      },
+      {
+        url: '#superior-suite',
+        title: 'Superior Suite',
+      },
+      {
+        url: '#deluxe-suite',
+        title: 'Deluxe Suite',
+      },
+      {
+        url: '#pool-villa',
+        title: 'Pool Villa',
+      },
+    ],
+    secondaryMenuHeading: 'Our Rooms',
+    bookNowLink: {
+      url: '#book-now',
+      title: 'Book Now',
+    },
+    socialIcons: [
+      {
+        url: '/images/icons/x-logo.svg',
+      },
+      {
+        url: '/images/icons/insta-logo.svg',
+      },
+      {
+        url: '/images/icons/fb-logo.svg',
+      },
+      {
+        url: '/images/icons/yt-logo.svg',
+      },
+    ],
+  };
+
   const { scrollY } = useScroll();
 
   useMotionValueEvent(scrollY, 'change', (latest) => {
@@ -94,7 +162,73 @@ const MenuModal = () => {
         )}
       </AnimatePresence>
 
-      <section className="menu-modal"></section>
+      <section className="menu-modal fixed top-0 right-0 h-screen w-full z-[99999] bg-black/50">
+        <div className="menu-modal__inner">
+          <button className="menu-modal__close h-[109px] w-[50px] flex flex-col gap-[10px] items-center justify-center hover:cursor-pointer group transition-all duration-300 ease-out">
+            <div className="close-icon h-[16px] w-[16px] flex flex-col justify-between items-end">
+              <span className="close-icon__line block h-[1px] w-full bg-white group-hover:w-[15px] transition-all duration-300 ease-out"></span>
+              <span className="close-icon__line block h-[1px] w-full bg-white group-hover:w-[32px] transition-all duration-300 ease-out"></span>
+              <span className="close-icon__line block h-[1px] w-full bg-white group-hover:w-[15px] transition-all duration-300 ease-out"></span>
+            </div>
+            <div className="close-text h-[39px]">
+              <span className="btn text-white rotate-[270deg] translate-y-[12px]">
+                Close
+              </span>
+            </div>
+          </button>
+          <div className="social-icons">
+            {data.socialIcons.length > 0 &&
+              data.socialIcons.map((item) => {
+                return <img src={item.url} alt="" className="social-icon" />;
+              })}
+          </div>
+          <div className="menu-modal__menu-wrapper relative">
+            <img
+              src={data.patternImageUrl}
+              alt=""
+              className="bg-img menu-modal__bg-img"
+            />
+            <div className="primary-nav">
+              {data.primaryMenu.length > 0 &&
+                data.primaryMenu.map((item) => {
+                  return (
+                    <a href={item.url} className="primary-nav__item nav-item">
+                      {item.title}
+                    </a>
+                  );
+                })}
+            </div>
+            <div className="secondary-nav">
+              <div className="secondary-nav__heading">Our Rooms</div>
+              <div className="secondary-nav__items">
+                {data.secondaryMenu.length > 0 &&
+                  data.secondaryMenu.map((item) => {
+                    return (
+                      <a
+                        href={item.url}
+                        className="secondary-nav__item nav-item"
+                      >
+                        {item.title}
+                      </a>
+                    );
+                  })}
+              </div>
+              {data.bookNowLink && (
+                <div className="btn-wrapper">
+                  <a
+                    href={data.bookNowLink.url}
+                    className="btn btn--rounded btn--roundedGold"
+                  >
+                    <span className="btn--rounded__text">
+                      {data.bookNowLink.title}
+                    </span>
+                  </a>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
